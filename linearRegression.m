3c70a8ba3133507a0d92804d67839377639e7fa4
unknown <alex.zhang9812@gmail.com>
2022-03-10 20:07:42 +0000

    .

--- linearRegression.m
+++ linearRegression.m
@@ -4,6 +4,6 @@ function [Parameter] = linearRegression(y, X)
 %Output: Parameter: matrix(nfeature,nlabel)
 %To calculate coefficient estimates for a model with constant terms (intercepts), 
 % include a column consisting of 1 in the matrix X.
-    Parameter = pinv(X*X.')*X*y.';
-%To calculate y: y = X.'*Parameter;
+    Parameter = transpose(pinv(X*X.')*X*y.');
+%To calculate y: y = Parameter*X;
 end
